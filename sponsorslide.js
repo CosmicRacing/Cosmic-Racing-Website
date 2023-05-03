9d52cb9aedfba7aa1ca51657ea07321220d2d425
CosmicRacing <91793115+CosmicRacing@users.noreply.github.com>
2023-05-03 18:23:33 +0300

    Add files via upload

---
+++ sponsorslide.js
@@ -0,0 +1,9 @@
+/*var container = document.getElementsByClassName("sponsor-image-container")[0];
+let i = 0;
+setInterval(function() {
+	i += 0.1
+    if (i >= 50){
+        i = 0;
+    }
+    container.style.left =  ((-i / 100) * container.offsetWidth)+ "px";
+}, 1);*/
---
+++ videoslide.js
@@ -0,0 +1,5 @@
+function videoClicked(){
+    var element = document.getElementById("thumbnail");
+    element.style.display = "none";
+    var videoplayer = document.getElementById("teamvideo").src += "?autoplay=1";
+}
