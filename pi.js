80688f08e0516edc6274321d50c666f25224e2d6
CosmicRacing <91793115+CosmicRacing@users.noreply.github.com>
2023-07-15 19:46:30 +0300

    Different animation pi

--- pi.js
+++ pi.js
@@ -7,14 +7,16 @@ async function loadMember(index){
         var memberContainer = document.getElementsByClassName("member-container")[currentMember];
         memberContainer.classList.remove("transform-second");
         memberContainer.classList.add("transform-first");
-        await sleep(500)
-        memberContainer.classList.add("hidden");
 
-        currentMember = index;
-        memberContainer = document.getElementsByClassName("member-container")[currentMember];
+
+        
+        memberContainer = document.getElementsByClassName("member-container")[index];
         memberContainer.classList.remove("hidden");
         memberContainer.classList.remove("transform-first");
         memberContainer.classList.add("transform-second");
-        
+        await sleep(500)
+        memberContainer = document.getElementsByClassName("member-container")[currentMember];
+        memberContainer.classList.add("hidden");
+        currentMember = index;
     }
 }
